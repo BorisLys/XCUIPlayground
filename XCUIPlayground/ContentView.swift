ad7a0255cc5c370a75c1b9d3778b60c822f9de8c
Boris Lysikov <b.lysikov@dodobrands.io>
2025-12-30 20:17:51 +0700

    first commit

---
+++ XCUIPlayground/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  XCUIPlayground
+//
+//  Created by Борис Лысиков on 30.12.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ XCUIPlayground/XCUIPlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  XCUIPlaygroundApp.swift
+//  XCUIPlayground
+//
+//  Created by Борис Лысиков on 30.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct XCUIPlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
