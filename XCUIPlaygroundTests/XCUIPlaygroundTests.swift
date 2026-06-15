b952bc726e18ed62c43b2d50a601172957bd0ac5
Boris Lysikov <b.lysikov@dodobrands.io>
2026-06-15 23:17:19 +0700

    Add XCUIPlaygroundTests unit test target

---
+++ XCUIPlaygroundTests/XCUIPlaygroundTests.swift
@@ -0,0 +1,18 @@
+//
+//  XCUIPlaygroundTests.swift
+//  XCUIPlaygroundTests
+//
+//  Created by Борис Лысиков on 15.06.2026.
+//
+
+import Testing
+
+struct XCUIPlaygroundTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
